6b3768313464539c48ee37dbcdced6fa6b23f934
Yuriy Boev <yuriy.boev@blue-leaf.co.uk>
2020-02-04 14:55:10 +0200

    Add sfcc-ci dependency

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
+const sfcc = require('sfcc-ci');
 
 async function run() {
     try {
         const sb = core.getInput('sandboxes');
+        const sf_username = core.getInput('sf_username');
+        const sf_password = core.getInput('sf_password');
+        const client_id = core.getInput('client_id');
+        const client_password = core.getInput('client_password');
+
         console.log(`SB data: ${sb}`);
 
-        const context = github.context;
-        console.log(context);
+        sfcc.auth.auth(client_id, client_password, (err, token) => {
+            if (token) {
+                console.log('Authentication succeeded. Token is %s', token);
+            }
+            if (err) {
+                console.error('Authentication error: %s', err);
+            }
+        });
 
-//         core.debug((new Date()).toTimeString());
+        // const context = github.context;
+        // console.log(context);
 
-//         core.setOutput('time', new Date().toTimeString());
+        core.setOutput('message', 'Finished');
     }
     catch (error) {
         core.setFailed(error.message);
