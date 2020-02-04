398fb2df1b2a2de1cf653e5dcdb3d5065aac7a3a
Yuriy Boev <yuriy.boev@gmail.com>
2020-02-04 13:01:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const core = require('@actions/core');
 
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
-        console.log(`Waiting ${ms} milliseconds...`);
+        const sb = core.getInput('sandboxes');
+        console.log(`SB data: ${sb}`);
 
         const context = github.context;
         console.log(context);
 
-        core.debug((new Date()).toTimeString());
+//         core.debug((new Date()).toTimeString());
 
-        core.setOutput('time', new Date().toTimeString());
+//         core.setOutput('time', new Date().toTimeString());
     }
     catch (error) {
         core.setFailed(error.message);
